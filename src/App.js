cb1397d3a3b95ca827973b1a9ccfbf3125835dd8
eliyab <eliyab1@gmail.com>
2021-10-04 15:55:44 -0400

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './NavBar';
+import Titles from "./Titles"
+export default function App() {
+  const {tabs, home, services, contact, aboutMe} = NavBar()
+  return <>
+    <Titles />
+    {tabs()}
+    {home && <h1>This is the Home Page</h1>}
+    {services && <h1>This is the Services Page</h1>}
+    {contact && <h1>This is the Contact Page</h1>}
+    {aboutMe && <h1>This is the About Me Page</h1>}
+    
+  </>
 }
-
-export default App;
---
+++ src/Titles.js
@@ -0,0 +1,4 @@
+export default function Header() {
+return  <div className="titles" ><h1 className="title">Mental Fitness Therapy</h1>
+    <h3 className="title">Ruthie Ayzenberg LMFT LPCC</h3>
+    <h4 className="title">Room 2 Grow</h4></div>}
