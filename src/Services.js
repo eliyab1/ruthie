6df51025a404c81ce6424811fd955ea28601b680
eliyab <eliyab1@gmail.com>
2021-10-10 21:17:25 -0400

    styled services page buttons a bit

--- src/Services.js
+++ src/Services.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 export default function Services() {
     useEffect(()=>{document.title="Services | Mental Fitness Therapy"},[])
     return <div className="servicesnav">
-        <div activeClassName="active" style={{"margin": "50px 200px 30px 199px"}}>
+        <div activeClassName="active" style={{"margin": "150px 200px 30px 199px"}}>
     <NavLink to="/services/individual"><button>Individual Therapy</button></NavLink>
     <NavLink to="/services/maternalhealth"><button>Maternal Mental Health</button></NavLink>
     <NavLink to="/services/curriculumdev"><button>Curriculum Development</button></NavLink>
