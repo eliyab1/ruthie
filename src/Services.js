45b6b7a0f76a9579a6acfb593ed2c297dfa5886b
eliyab <eliyab1@gmail.com>
2021-10-10 21:08:00 -0400

    added bootstrap and fixed services pg

--- src/Services.js
+++ src/Services.js
@@ -1,7 +1,12 @@
 import { useEffect } from "react"
+import { NavLink } from "react-router-dom"
 export default function Services() {
     useEffect(()=>{document.title="Services | Mental Fitness Therapy"},[])
-    return <p>--------------------------------------------------------------------------------------
-        ----------------------------------------------------------------------------------------------
-        -------------------------------------------------------------------------------------------------------------</p>
+    return <div className="servicesnav">
+        <div activeClassName="active" style={{"margin": "50px 200px 30px 199px"}}>
+    <NavLink to="/services/individual"><button>Individual Therapy</button></NavLink>
+    <NavLink to="/services/maternalhealth"><button>Maternal Mental Health</button></NavLink>
+    <NavLink to="/services/curriculumdev"><button>Curriculum Development</button></NavLink>
+    <NavLink to="/services/grouptherapy"><button>Group Therapy</button></NavLink>
+</div></div>
 }
