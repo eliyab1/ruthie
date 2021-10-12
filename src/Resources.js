942e146c9f50b2d15706af56d3313ef832e5064c
eliyab <eliyab1@gmail.com>
2021-10-11 21:37:40 -0400

    changed podcast images and added link and styled on resources

--- src/Resources.js
+++ src/Resources.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
 import spotify from "./spotify.png"
-import apple from "./apple.jpg"
+import apple from "./apple.png"
 import audible from "./audible.png"
+import google from "./google.png"
 export default function Resources() {
-    useEffect(()=>{document.title="Resources | Mental Fitness Therapy"},[])
+    useEffect(()=>{document.title="Resources  Mental Fitness Therapy"},[])
     return <>
             <div className="page-content"><h2><u>Pregnancy and Postpartum</u></h2>
                 <div className="resources">
@@ -19,33 +20,73 @@ export default function Resources() {
                 </div>
             </div>
 
-            <div className="page-content"><h2><u>PODCASTS</u></h2>
+            <div className="page-content">
+                <h2>
+                    <u>PODCASTS</u>
+                </h2>
                 <div className="resources">
-                    <h3>Tara Brach  <a href="https://open.spotify.com/show/37McjD0j2cdu4GExcFQgm0" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/tara-brach/id265264862" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/author/Tara-Brach/B001KE8BHO" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3>
-                    <h3>Where Should We Begin?  <a href="https://open.spotify.com/show/3fKOTwtnX5oZLaiNntKWAV" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/where-should-we-begin-with-esther-perel/id1237931798" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/pd/Where-Should-We-Begin-with-Esther-Perel-Podcast/B08DDCJ44L" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3>
-                    <h3>Shrink Rap Radio  <a href="https://open.spotify.com/show/3pfGEC84F7SJZLUmDaYidv" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/shrink-rap-radio/id79491957" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/pd/Podcast/B08JJNYVV1" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3>
-                    <h3>Better Sex Podcast  <a href="https://open.spotify.com/show/6W4PJ6NB8akNyzaGUZGW0X" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/better-sex/id1329973865" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/author/Jessa-Zimmerman/B07G1T8Y16?ref=a_author_Je_c19_lAuthor_1_1_1&amp;pf_rd_p=1ae0e65e-ad09-4aa7-aa73-772cefb1b5e1&amp;pf_rd_r=G1NHM3CN96S7R6NQX0Z9" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3>
-                    <h3>Attachment Theory in Action  <a href="https://open.spotify.com/show/72FDvKx9GoPXApdaT1VdfH" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/tr/podcast/attachment-theory-in-action-with-karen-doyle-buckwalter/id1305291387" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/pd/Attachment-Theory-in-Action-with-Karen-Doyle-Buckwalter-Podcast/B08JJNG8SC" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3>
-                    <h3>The Skillful Podcast  <a href="https://open.spotify.com/show/0CYO75hjiJIBstRpvWZKCa?autoplay=true" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/the-skillful-podcast/id1461774020" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a></h3>
-                    <h3>The Mindful Podcast  </h3>
-                        {/* <a href="https://open.spotify.com/show/37McjD0j2cdu4GExcFQgm0" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
-                                    |<a href="https://podcasts.apple.com/us/podcast/tara-brach/id265264862" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="30" /></a>
-                                    |<a href="https://www.audible.com/author/Tara-Brach/B001KE8BHO" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="35" /></a></h3> */}
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>Tara Brach</h2>
+                            <div style={{"display":"flex","justify-content":"space-between", "column-gap":"20px"}}>
+                                <a href="https://open.spotify.com/show/37McjD0j2cdu4GExcFQgm0" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/us/podcast/tara-brach/id265264862" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cDovL3RhcmFicmFjaC5saWJzeW4uY29tL3Jzcw==" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                                <a href="https://www.audible.com/author/Tara-Brach/B001KE8BHO" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="41" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>Where Should We Begin?</h2>
+                            <div style={{"display":"flex","justify-content":"space-between"}}>
+                                <a href="https://open.spotify.com/show/3fKOTwtnX5oZLaiNntKWAV" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/us/podcast/where-should-we-begin-with-esther-perel/id1237931798" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cDovL2VzdGhlci5hdWRpYmxlLmxpYnN5bnByby5jb20vcnNz" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                                <a href="https://www.audible.com/pd/Where-Should-We-Begin-with-Esther-Perel-Podcast/B08DDCJ44L" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="41" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>Shrink Rap Radio</h2>
+                            <div style={{"display":"flex","justify-content":"space-between"}}>
+                                <a href="https://open.spotify.com/show/3pfGEC84F7SJZLUmDaYidv" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/us/podcast/shrink-rap-radio/id79491957" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5wb2RjYXN0bWlycm9yLmNvbS9zaHJpbmstcmFwLXJhZGlv" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                                <a href="https://www.audible.com/pd/Podcast/B08JJNYVV1" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="41" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>Better Sex Podcast</h2>
+                            <div style={{"display":"flex","justify-content":"space-between"}}>
+                                <a href="https://open.spotify.com/show/6W4PJ6NB8akNyzaGUZGW0X" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/us/podcast/better-sex/id1329973865" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvMjc5NDE3Mi9lcGlzb2Rlcy9mZWVk" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                                <a href="https://www.audible.com/author/Jessa-Zimmerman/B07G1T8Y16?ref=a_author_Je_c19_lAuthor_1_1_1&amp;pf_rd_p=1ae0e65e-ad09-4aa7-aa73-772cefb1b5e1&amp;pf_rd_r=G1NHM3CN96S7R6NQX0Z9" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="41" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>Attachment Theory in Action</h2>
+                            <div style={{"display":"flex","justify-content":"space-between"}}> 
+                                <a href="https://open.spotify.com/show/72FDvKx9GoPXApdaT1VdfH" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/tr/podcast/attachment-theory-in-action-with-karen-doyle-buckwalter/id1305291387" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hdHRhY2htZW50dGhlb3J5aW5hY3Rpb24ucG9kYmVhbi5jb20vZmVlZC54bWw" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                                <a href="https://www.audible.com/pd/Attachment-Theory-in-Action-with-Karen-Doyle-Buckwalter-Podcast/B08JJNG8SC" rel="noreferrer" target="_blank"><img src={audible} alt="audible link" height="41" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>The Skillful Podcast</h2>
+                            <div style={{"display":"flex","justify-content":"center", "columnGap":"20px"}}>
+                                <a href="https://open.spotify.com/show/0CYO75hjiJIBstRpvWZKCa?autoplay=true" rel="noreferrer" target="_blank"><img src={spotify} alt="spotify link" height="40" /></a>
+                                <a href="https://podcasts.apple.com/us/podcast/the-skillful-podcast/id1461774020" rel="noreferrer" target="_blank"><img src={apple} alt="apple link" height="40" /></a>
+                                <a href="https://podcasts.google.com/feed/aHR0cHM6Ly90aGVza2lsbGZ1bGxwb2RjYXN0LmxpYnN5bi5jb20vcnNz" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                            </div>
+                    </div>
+                    <div style={{"background-color":"MediumAquaMarine","padding":"2px 20px 20px","border-radius":"10px","margin-bottom":"25px"}}>
+                        <h2>The Mindful Podcast</h2>
+                            <div style={{"display":"flex","justify-content":"center"}}>
+                            <a href="https://podcasts.google.com/feed/aHR0cDovL2ZlZWRzLnNvdW5kY2xvdWQuY29tL3VzZXJzL3NvdW5kY2xvdWQ6dXNlcnM6NjUwMTY5MTYvc291bmRzLnJzcw?ep=14" rel="noreferrer" target="_blank"><img src={google} alt="google link" height="40" /></a>
+                            </div>
+                    </div>
                     <h3>MORE STUFF TBD….</h3>
                 </div>
             </div>
-
             <div className="page-content"><h2><u>BOOKS</u></h2>
                 <div className="resources">
                     <h3>Attached</h3>
