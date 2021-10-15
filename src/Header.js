532903206f06ca55959cc5d717e96c4dca6230b2
eliyab <eliyab1@gmail.com>
2021-10-15 00:41:26 -0400

    left margin on logo

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ return  <>  <div className="header">
                     <h3 style={{"marginLeft":"15%"}}>Your Psychological Gym</h3>
                 </div>
                 <div >
-                    <img src={test1} width="50%" alt="logo" style={{"display":"block","marginLeft":"auto","marginRight":"auto","borderRadius":"85%"}} />
+                    <img src={test1} width="50%" alt="logo" style={{"display":"block","marginLeft":"33%","marginRight":"auto","borderRadius":"85%"}} />
                 </div>
                 <div>
                     <h2  >Ruthie Ayzenberg LMFT LPCC PMH-C</h2>
