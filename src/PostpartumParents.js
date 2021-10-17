7d56fbf35034687a7fc079164ec8e01e890a9291
eliyab <eliyab1@gmail.com>
2021-10-16 22:35:51 -0400

    updated postpartum parents content

--- src/PostpartumParents.js
+++ src/PostpartumParents.js
@@ -3,14 +3,18 @@ export default function PostpartumParents() {
     useEffect(()=>{document.title="Postpartum Parents | Mental Fitness Therapy"},[])
     return <>
     <div className="page-content">
-        <p>Postpartum depression and anxiety is notoriously common today. One in 7 new moms 
-            meets criteria for postpartum depression and 1 in 10 new dads. The pre and postpartum time is an 
-            optimal window to take care of new or pre-existing depression and anxiety, while the family is still in the early 
-            stages of adjusting to the new baby. If depression and anxiety go untreated, they usually get worse over time and can 
-            make parenting (and life in general) much more difficult. The good news is that there is a lot you can do to help prevent 
-            postpartum depression and anxiety or reverse it in its tracks.</p>
+        <p>Postpartum depression and anxiety are the most common complications of pregnancy.  One in seven new moms meets criteria for postpartum depression, 
+            which often co-occurs with anxiety. Many mothers feel that something is wrong, but don’t speak up or ask for help. Unfortunately, maternal depression 
+            is underscreened in doctor offices, leaving many mothers to suffer in silence without understanding what’s going on. There are many factors that 
+            contribute to depression and anxiety--hormones, sleep scarcity, lack of support, epigenetic predispositions, etc--the symptoms are NEVER the mother’s 
+            fault. However, they do need attention and getting treatment can help make a u-turn sooner rather than later. If depression and anxiety go untreated, 
+            they usually get worse over time and can make parenting (and life in general) much more difficult than it has to be.</p>
 
-        <p>Here are some red flags to watch out for. If you’re having any of these signs for more than a couple days, then I encourage you to seek help.</p>
+        <p>The good news is that there is a lot you can do to help prevent postpartum depression and anxiety or reverse it in its tracks.  The pre and postpartum 
+            time is an optimal window to take care of new or pre-existing depression and anxiety, while the family is still in the early stages of adjusting to the 
+            new baby.The best gift you can give to your family is your mental health.</p>
+
+        <p>Depression and anxiety can show up in many ways. Here are some red flags to watch out for.</p>
 
         <ul style={{"listStyleType":"none"}}>
             <li>-Low mood, sadness, or tearfulness</li>
@@ -40,13 +44,13 @@ export default function PostpartumParents() {
             <li>-Symptoms often  wax and wane</li>
         </ul>
 
-    <p>*Postpartum psychosis is a psychiatric emergency that needs immediate evaluation and treatment. </p><br /><br />
+    <p>*Postpartum psychosis is a psychiatric emergency that needs immediate evaluation and treatment. </p><br />
 
 
-    <p>If you’re having any of these signs for more than a couple days, then I encourage you to seek help. The best gift you can give to your family is your mental health.</p>
+    <p>If you’re having any of these signs for more than a couple days, then I encourage you to seek help.</p>
     <br />
-    <ul>Here are some things I can help you with: 
-        <li>Assessment, diagnosis, and treatment of Pperinatal mood and anxiety disorders (depression, anxiety, bipolar, OCD, psychosis)</li>
+    <ul>Here are some things I can help you with:<br />
+        <li>Assessment, diagnosis, and treatment of perinatal mood and anxiety disorders (depression, anxiety, bipolar, OCD, psychosis)</li>
         <li>Birth and postpartum planning for new parents/families</li>
         <li>Postpartum adjustment for new moms/dads/families</li>
         <li>Pregnancy after infertility</li>
