9cbfa36fdca5baa11b42fe25652efe7cf0c19499
eliyab <eliyab1@gmail.com>
2021-10-19 09:50:37 -0400

    updated content about me, individual-w css

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -11,15 +11,16 @@ export default function AboutMe() {
    <br />
     <h3>I am a licensed Marriage and Family Therapist and Professional Clinical Counselor,  practicing therapy since 2013. I completed 
         the Counseling Psychology program at Santa Clara University, and over the years have worked with kids, teens, families, and 
-        adults with a wide range of concerns across all levels of care from outpatient to hospital.  Working in the MOMS program at 
-        El Camino Hospital, getting training from Postpartum Support International, and being a mother of two, has groomed me into 
-        specializing in maternal mental health, including working with moms and dads facing pre/postpartum issues. I also enjoy 
-        working with students, professionals, people with diverse backgrounds, and anyone who is motivated to do the therapy work.</h3>
+        adults with a wide range of concerns across all levels of care from outpatient to hospital.  Working in the MOMS program at El 
+        Camino Hospital, getting training from Postpartum Support International, and being a mother of two, has equipped me to specialize 
+        in maternal and perinatal mental health, including working with moms and dads facing difficulties during pregnancy and postpartum. 
+        I also enjoy working with students, professionals, people with diverse backgrounds, and anyone who is motivated to do the therapy 
+        work. I welcome people with situations from the most straightforward to the most unusual.</h3>
      
     <h3>There’s a myth that therapy cannot be fun. The reality is that laughing, lightheartedness, creativity and joy have a space in the 
-    therapy room as much as the difficult emotions.  My goal is to provide you with therapy that is focused, effective, and also fun 
-    so that you can bring out your best self. To do this, we orient therapy around 3 pillars: emotional and cognitive processing, 
-    practical skill-building, and integrating therapy material into your daily life. We use a variety of techniques, depending on 
+    therapy room as much as the difficult emotions.  My goal is to provide you with therapy that is focused, effective, and also fun. To 
+    do this, we orient therapy around 3 pillars: emotional and cognitive processing, practical skill-building, and integrating therapy 
+    material into your daily life. We use a variety of techniques, depending on 
     your needs and preferences. Some modalities I use (most of which are evidence-based) are Short-Term Psychodynamic Therapy, 
     Cognitive Behavioral Therapy (CBT), Dialectical Behavioral Therapy (DBT), Internal Family Systems (IFS), Art Therapy, 
     Attachment-Based, Interpersonal Psychotherapy (IPT), and Trauma-Informed Care.</h3>
--- src/IndividualTherapy.js
+++ src/IndividualTherapy.js
@@ -2,43 +2,75 @@ import { useEffect } from "react"
 export default function IndividualTherapy() {
     useEffect(()=>{document.title="Individual Therapy | Mental Fitness Therapy"},[])
     return <>
-    <div className="page-content">
-        <h2>“If you are your own problem, then you can be your own solution.“</h2><br />
-        <ul>
-            <li>
-                Issues: <ul>
+        <div className="individualDiv">
+            <h2 style={{ "textAlign":"center","marginTop":"4%" }}>“If you are your own problem, then you can be your own solution.“</h2><br />
+            <ul style={{ "margin":"4% 18% 8% 18%" }}>
+                <li>Issues: </li>
+                <ul>
                     <li>Depression</li>
                     <li>Anxiety</li>
                     <li>Bipolar</li>
                     <li>Relationship difficulties</li>
                     <li>Life transitions</li>
                     <li>Career and workplace issues</li>
+                    <li>Sleep issues</li>
                     <li>Identity crisis</li>
+                    <li>Trauma</li>
                 </ul>
-            </li>
-            <li>
-            Modalities: <ul>
-                <li>Short-Term Dynamic Psychotherapy (STDP)</li>
-                <li>Internal Family Systems (IFS) </li>
-                <li>Interpersonal Therapy (IPT)</li>
-                <li>Attachment-Based Therapy</li>
-                <li>Cognitive Behavioural Therapy (CBT)</li>
-                <li>Dialectical Behavioral Therapy (DBT)</li>
-                <li>Expressive Arts</li>
-                <li>Emotional Freedom Technique (EFT-Tapping)</li>
-
+                <hr />
+                <li>Modalities: </li>
+                <ul>
+                    <li>Short-Term Dynamic Psychotherapy (STDP)</li>
+                    <li>Internal Family Systems (IFS) </li>
+                    <li>Interpersonal Therapy (IPT)</li>
+                    <li>Attachment-Based Therapy</li>
+                    <li>Cognitive Behavioural Therapy (CBT): </li>
+                </ul>
+                <p>CBT is an evidence-based modality that focuses on the connection 
+                    between thoughts, feelings, and behaviors. The premise of CBT is that our thoughts influence our feelings and if 
+                    we can change our thoughts to be more helpful, then we can change our feelings too. Our brain is programmed to 
+                    automatically produce thoughts, many of which are neither helpful nor useful nor productive. When our brain produces 
+                    too many automatic negative thoughts (ANTS), we can fall into depression or constant worrying. The  ANTS may feel 
+                    true in the moment, but when we examine them under the microscope, we can see that they are actually quite distorted. 
+                    We can then revise the unhelpful thoughts to be more realistic and helpful. CBT has many tools for editing unhelpful 
+                    thoughts and beliefs and jumpstarting new behaviors.  Since CBT targets primarily the conscious regions of the brain 
+                    and often does not reach the deeper unconscious layers, I use it in conjunction with other approaches that focus more 
+                    on deeper brain healing. Learning CBT skills is like learning the therapy alphabet --they are foundational for daily 
+                    life management and building capacity for deeper emotional work.</p>
+                <ul>
+                    <li>Dialectical Behavioral Therapy (DBT): </li>
+                </ul>
+                <p>DBT is an evidence-based treatment that is a  blend of eastern philosophy and western psychology. It is used to help 
+                    people with managing emotions and relationships by teaching a plethora of skills in the following four areas: 
+                    mindfulness, distress tolerance, emotional regulation, and interpersonal effectiveness. One of the main tenets of 
+                    DBT is the idea of dialectics, which is a process for reconciling two opposites and finding some kind of synthesis or 
+                    middle ground solution.  What is remarkable about DBT is its amazing repertoire of skills and tools, which are organized 
+                    into neat categories, acronyms, and sequences. Like CBT, the DBT skills are foundational for increasing emotional strength 
+                    and can go a long way on their own. They are very useful for people wanting to work through more activating material without 
+                    getting overwhelmed. I make an effort to incorporate DBT skills organically into the conversations and support you with 
+                    reinforcing the skills in the sessions and for homework.</p>
+                <ul>
+                    <li>Expressive Arts</li>
+                </ul>
+                <ul>
+                    <li>Emotional Freedom Technique (EFT-Tapping): </li>
+                </ul>
+                <p>EFT is a holistic healing technique that has been around for thousands of years in Asia and was formalized in the USA about 
+                    30 years ago. EFT harnesses the power of acupressure, which is like acupuncture without the needles. It works by systematically 
+                    tapping on pressure points on the body that correspond to many parasympathetic nerve endings (which help you relax).  The 
+                    combination of tapping while mindfully processing distressing material helps integrate emotions and thoughts with the body to 
+                    reduce distress about the problem at hand. EFT has been shown to be helpful for anxiety, irritability, difficult emotions, 
+                    physical pain, insomnia, phobias, and trauma activation. This is a great tool to practice in the sessions and reinforce at home. 
+                    The more time your nervous system can spend in a balanced state, the easier it will be to rebalance yourself when triggers occur.</p>
+                <hr />
+                <li>What to expect: </li>
+                <p style={{ "marginLeft":"2%" }}>Throughout treatment I will remain active, supportive, and practical. To accelerate treatment, I use evidence-based methods to help 
+                    you build emotional capacity to let go of self-defeating behaviors in order to access and work through the feelings that are causing 
+                    your distress so that you can experience greater emotional freedom and symptom resolution. To this effect, I will challenge you in 
+                    identifying  behaviors that are unhelpful to you and encourage you to turn toward healthier ways of relating with yourself and others. 
+                    Since having a strong alliance is essential for  therapeutic success, I encourage you to provide ongoing honest feedback  about what 
+                    is working for you and what is not so that we can make the necessary changes.</p>
             </ul>
-            </li>
-            <li>
-            What to expect:
-            </li>
-            <p>Throughout treatment I will remain active, supportive, and practical. To accelerate treatment, I use evidence-based methods to help 
-                you build emotional capacity to let go of self-defeating behaviors in order to access and work through the feelings that are causing 
-                your distress so that you can experience greater emotional freedom and symptom resolution. To this effect, I will challenge you in 
-                identifying  behaviors that are unhelpful to you and encourage you to turn toward healthier ways of relating with yourself and others. 
-                Since having a strong alliance is essential for  therapeutic success, I encourage you to provide ongoing honest feedback  about what 
-                is working for you and what is not so that we can make the necessary changes.</p>
-        </ul>
-    </div>
+        </div>
     </>
 }
