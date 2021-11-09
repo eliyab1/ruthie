3d0b74c7ab61a64b6a8d8c1ddc6ca07873d46331
eliyab <eliyab1@gmail.com>
2021-11-09 09:45:05 -0500

    removed services page

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ export default function NavBar() {
     return <div className="topnav">
         <NavLink   exact to="/" ><button activeStyle={{ "color":"dodgerblue" }}>Home</button></NavLink>
         <div className="dropdown">
-            <NavLink activeStyle={{ "color":"dodgerblue" }} to="/services" ><button className="dropdown">Services</button></NavLink>
+            <button className="dropdown">Services</button>
                 <div className="dropdown-content">
                     <NavLink activeStyle={{ "color":"dodgerblue" }} to="/services/individual"><span>Individual Therapy</span></NavLink>
                     <NavLink activeStyle={{ "color":"dodgerblue" }} to="/services/postpartumparents"><span>Postpartum Parents</span></NavLink>
